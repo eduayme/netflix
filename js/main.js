c4343f947fcd33fd5282c335d8f7142bb6b94dfe
Eduard Aymerich <eduardaymerich@gmail.com>
2020-02-22 02:07:23 +0100

    Movies explore started

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
     // change navbar background after start scrolling
+    background_nav();
+
     $(document).scroll(function() {
+        background_nav();
+    });
+
+    function background_nav() {
         if($(window).scrollTop() >= 50) {
             $('#navbar').css('background-color', '#141414');
         } else {
             $('#navbar').css('background-color', 'transparent');
         }
-    });
+    }
 
 });
