7adb6052e4f67ee14ebe1df67d170c28eb35c149
Eduard Aymerich <eduardaymerich@gmail.com>
2020-02-24 01:03:51 +0100

    Finished filtering and sorting

--- js/main.js
+++ js/main.js
@@ -29,4 +29,42 @@ $(document).ready(function() {
         });
     });
 
+    function collision($div1, $div2) {
+        var x1 = $div1.offset().left;
+        var w1 = 40;
+        var r1 = x1 + w1;
+        var x2 = $div2.offset().left;
+        var w2 = 40;
+        var r2 = x2 + w2;
+
+        if (r1 < x2 || x1 > r2) return false;
+        return true;
+
+    }
+
+    // // slider call
+
+    $('#slider').slider({
+        range: true,
+        min: 0,
+        max: 10.1,
+        step: 0.1,
+        values: [ 0, 10.1 ],
+        slide: function(event, ui) {
+
+            $('.ui-slider-handle:eq(0) .price-range-min').html(ui.values[ 0 ]+'' );
+            $('.ui-slider-handle:eq(1) .price-range-max').html(ui.values[ 1 ]);
+
+            if (collision($('.price-range-min'), $('.price-range-max')) == true) {
+                $('.price-range-min, .price-range-max').css('opacity', '0');
+            } else {
+                $('.price-range-min, .price-range-max').css('opacity', '1');
+            }
+
+        }
+    });
+
+    $('.ui-slider-handle:eq(0)').append('<span class="price-range-min value">' + $('#slider').slider('values', 0 ) + '</span>');
+    $('.ui-slider-handle:eq(1)').append('<span class="price-range-max value">' + $('#slider').slider('values', 1 ) + '</span>');
+
 });
